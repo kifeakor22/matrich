1124adb0a654aeca291ae6508eea2b03e43e7f0e
Kc <kifeakor@yahoo.com>
2023-10-04 10:51:53 +0100

    footer added

---
+++ src/components/footer/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './Footer.css'; // Import your CSS file for styling
+
+const Footer = () => {
+  return (
+    <footer className="footer-container">
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img src={require('./images/logo.jpg')} alt="Company Logo" />
+          <h2>Matrich</h2>
+        </div>
+        <div className="footer-links">
+          <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About Us</a></li>
+            <li><a href="/services">Services</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+        </div>
+        <div className="footer-social">
+          <h3>Follow Us</h3>
+          <ul>
+            <li><a href="#"><i className="fab fa-facebook"></i></a></li>
+            <li><a href="#"><i className="fab fa-twitter"></i></a></li>
+            <li><a href="#"><i className="fab fa-instagram"></i></a></li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; {new Date().getFullYear()} DevAlter.io All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Herogrid from '../components/About';
 import ServiceCards from '../components/services';
+import Footer from '../components/footer';
 
 const Home = () => {
   return (
     <div>
       <Herogrid/>
       <ServiceCards/>
+      <Footer/>
     </div>
   );
 };
