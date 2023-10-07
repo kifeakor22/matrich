c57a99b5d6eda1925e8882864d438d40616284f4
Kc <kifeakor@yahoo.com>
2023-10-07 15:32:09 +0100

    modified navBar Toggler

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,64 +1,73 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; // Updated import path
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import { Link } from 'react-router-dom';
-import logo from './images/logo.jpg';
 
 const Nav = () => {
+  // State to control the Navbar collapse
+  const [navbarExpanded, setNavbarExpanded] = useState(false);
+
+  // Function to close the Navbar when a nav item is clicked
+  const closeNavbar = () => {
+    if (navbarExpanded) {
+      setNavbarExpanded(false);
+    }
+  };
+
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid"> {/* Wrap the navbar content in a container */}
-          <a className="navbar-brand" href="/">
-            Matrich
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse" // Use 'data-bs-toggle' for Bootstrap 5
-            data-target="#navbarTogglerDemo03"
-            aria-controls="navbarTogglerDemo03"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
-            <ul className="navbar-nav ms-auto"> {/* Use 'ul' for list of links */}
-              <li className="nav-item">
-                <Link className="link" to="about">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Matrich
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          onClick={() => setNavbarExpanded(!navbarExpanded)} // Toggle Navbar collapse state
+          aria-controls="navbarTogglerDemo03"
+          aria-expanded={navbarExpanded}
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className={`collapse navbar-collapse ${navbarExpanded ? 'show' : ''}`} // Add 'show' class when Navbar is expanded
+          id="navbarTogglerDemo03"
+        >
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="link" to="about" onClick={closeNavbar}> {/* Close Navbar on click */}
                 <a className="nav-link" href="#">
                   About Us
                 </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="link" to="support">
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="link" to="support" onClick={closeNavbar}> {/* Close Navbar on click */}
                 <a className="nav-link" href="#">
                   Supported Living
                 </a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Specialist Adult Care
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Senior Living
-                </a>
-              </li>
-            </ul>
-            <Link className="link" to="contact">
-              <button className="btn btn-dark enquire" type="button">
-                Make an Enquiry
-              </button>
-            </Link>
-          </div>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Specialist Adult Care
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Senior Living
+              </a>
+            </li>
+          </ul>
+          <Link className="link" to="contact">
+            <button className="btn btn-dark enquire" type="button">
+              Make an Enquiry
+            </button>
+          </Link>
         </div>
-      </nav>
-    </>
+      </div>
+    </nav>
   );
 };
 
