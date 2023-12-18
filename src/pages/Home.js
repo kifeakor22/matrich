c4c339e0e018bba67da6355e3eb46d54efcb82d9
Kc <kifeakor@yahoo.com>
2023-12-18 16:35:11 +0000

    added sections to home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,19 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Herogrid from '../components/About';
 import ServiceCards from '../components/services';
+import Contact from '../components/contact';
+import SupportedLiving from '../components/supportedliving';
+import SpecialistAdultCare from '../components/adultcare';
 import Footer from '../components/footer';
+import AboutUs from '../components/AboutUs';
 
 const Home = () => {
   return (
     <div>
       <Herogrid/>
       <ServiceCards/>
-      
+      <SupportedLiving/>
+      <Contact/>
     </div>
   );
 };
